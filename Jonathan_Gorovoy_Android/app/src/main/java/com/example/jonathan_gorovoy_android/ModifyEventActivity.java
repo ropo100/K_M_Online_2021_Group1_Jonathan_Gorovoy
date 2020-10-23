3c9d7312efd11de3a0b15a354dce632900a86552
ropo100 <ropogwrooi@gmail.com>
2020-10-23 18:53:42 +0300

    Added empty activity for every activity in the ScreenFlow diagram

---
+++ Jonathan_Gorovoy_Android/app/src/main/java/com/example/jonathan_gorovoy_android/ModifyEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.jonathan_gorovoy_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_event);
+    }
+}
