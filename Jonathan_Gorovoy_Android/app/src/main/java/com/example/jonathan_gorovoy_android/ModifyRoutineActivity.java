7a3339f3128b1e807f0b48412d79a9b64f73183d
JonathanGorovoy <jonathan.gorovoy@gmail.com>
2021-01-08 09:52:40 +0200

    Xml designs

--- Jonathan_Gorovoy_Android/app/src/main/java/com/example/jonathan_gorovoy_android/ModifyRoutineActivity.java
+++ Jonathan_Gorovoy_Android/app/src/main/java/com/example/jonathan_gorovoy_android/ModifyRoutineActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class ModifyRoutineActivity extends AppCompatActivity {
 
     Button btn1;
+    Button btn2;
     int eventIndex, isSpecificDay;
 
     @Override
@@ -18,18 +19,25 @@ public class ModifyRoutineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_modify_routine);
 
         btn1=(Button)findViewById(R.id.button31);
+        btn2=(Button)findViewById(R.id.button32);
 
         btn1.setOnClickListener(this::onClick);
+        btn2.setOnClickListener(this::onClick);
     }
 
     public void onClick(View view) {
         Intent i;
         switch(view.getId()) {
             case R.id.button31:
-                i = new Intent(this, SettingsActivity.class);
+                i = new Intent(this, ModifyEventActivity.class);
                 i.putExtra("source_activity", "activity_modify_routine");
                 i.putExtra("eventIndex", eventIndex);
                 i.putExtra("isSpecificDay", isSpecificDay);
+                startActivity(i);
+                break;
+            case R.id.button32:
+                i = new Intent(this, ViewRoutinesActivity.class);
+                i.putExtra("source_activity", "activity_modify_routine");
                 startActivity(i);
                 break;
         }
